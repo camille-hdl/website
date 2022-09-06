5fae1f461d90891ddf456747bb3e85ef0cb30b44
Camille Hodoul <camille.hodoul@gmail.com>
2022-09-06 22:54:53 +0200

    seneca quote on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,6 +38,21 @@ class BlogIndex extends React.Component {
             </div>
           )
         })}
+        <section id="seneca-quote">
+          <blockquote>
+          <p>Nothing will ever please me, no matter how excellent or
+          beneficial, if I must retain the knowledge of it to myself.
+          </p>
+          <p>And if wisdom were given me under the express condition
+          that it must be kept hidden and not uttered, I should refuse
+          it.</p>
+          <p>No good thing is pleasant to possess, without friends to
+          share it.</p>
+          <cite>Letters From a Stoic,
+            Lucius Annaeus Seneca</cite>
+          </blockquote>
+
+        </section>
       </Layout>
     )
   }
