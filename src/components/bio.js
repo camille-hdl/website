1d538990f64e12afa78995a643f6d82a1d4cbc39
Camille Hodoul <camille.hodoul@gmail.com>
2023-07-04 10:41:32 +0200

    new profile pic

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,7 +14,7 @@ import { rhythm } from "../utils/typography"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/squircletest.png/" }) {
+      avatar: file(absolutePath: { regex: "/profile-jardin-72.png/" }) {
         childImageSharp {
           fixed(width: 72, height: 72) {
             ...GatsbyImageSharpFixed
